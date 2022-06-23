a6183dd8dc0af7c3665f5bd3fcab04392a83be79
a_kiritani <a.kiritani@skillengine.jp>
2022-06-23 23:41:39 +0900

    headerのデザイン作成

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,31 @@
+// 画像をそれぞれimportします。パスに注意してください。
+import CoverImage from '../images/cover-image.jpg';
+import ProfileImage from '../images/profile-image.png';
+
 export const Header = () => {
-  // const componentName = 'Header';
-  const componentName = () => 'Header';
   return (
-    <div className="test-wrapper">
-      テスト
-      このコンポーネントの名前は{componentName()}です。
-    </div>
+    <header className="main-cover" style={{backgroundImage: `url(${CoverImage})`}}>
+      {/* overlayはカバー画像の上に透過して表示される背景要素です */}
+      <div className="overlay"></div>
+      <div className="container">
+        <div className="display-table">
+          <div className="display-table-contents">
+            {/* カバー画像 */}
+            <div className="profile-thumb" style={{backgroundImage: `url(${ProfileImage})`}}></div>
+            {/* 名前と肩書はみなさんのお名前や肩書を自由に入れてください */}
+            <h1 className="title-text">Atsu</h1>
+            <h3 className="title-text">Engineer</h3>
+            <ul className="social-icons">
+              <li className="icon-link">
+                {/* TODO */}
+              </li>
+              <li className="icon-link">
+                {/* TODO */}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
