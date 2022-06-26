11362ec4ee6c19cf577b7fa5ba9567d7f47019b2
a_kiritani <a.kiritani@skillengine.jp>
2022-06-26 23:49:56 +0900

    コンタクトページの修正

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,9 @@ export const Contact = () => {
           <h2>Contact to Me!</h2>
           <p className="description">お仕事のご相談・ご依頼など下記よりご連絡ください。</p>
           <a href="https://twitter.com/" className="btn">Contact Us</a>
+          <h2>Contact to Me!</h2>
+          <p className="description">お仕事のご相談・ご依頼など下記よりご連絡ください。</p>
+          <a href="https://twitter.com/" className="btn">Contact Us</a>
         </div>
       </div>
     </div>
