50c0fee150d55343449a3b5e64dab0e174270ce5
a_kiritani <a.kiritani@skillengine.jp>
2022-09-23 23:51:14 +0900

    useEffectの作成

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,34 @@
-import { useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
+import axios from 'axios';
+import { skillReducer, initialState, actionTypes } from '../reducers/skillReducer';
+
 
 export const Skills = () => {
-  const outputTest = () => { console.log('TEST'); };
- 
-  useEffect(outputTest);
+  const [state, dispatch] = useReducer(skillReducer, initialState);
+  useEffect(() => {
+    dispatch({ type: actionTypes.fetch });
+    axios.get('https://api.github.com/users/atsu0203/repos')
+      .then((response) => {
+        const languageList = response.data.map(res => res.language);
+        const countedLanguageList = generateLanguageCountObj(languageList);
+        dispatch({ type: actionTypes.success, payload: { languageList: countedLanguageList } });
+      })
+      .catch(() => {
+        dispatch({ type: actionTypes.error });
+      });
+  }, []);
+
+  const generateLanguageCountObj = (allLanguageList) => {
+    const notNullLanguageList = allLanguageList.filter(language => language != null);
+    const uniqueLanguageList = [...new Set(notNullLanguageList)];
+
+    return uniqueLanguageList.map(item => {
+      return {
+        language: item,
+        count: allLanguageList.filter(language => language === item).length
+      }
+    });
+  };
  
   return (
     <div id="skills">
